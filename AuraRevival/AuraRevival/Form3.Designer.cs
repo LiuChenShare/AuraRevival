ac86bec366926c2f08636c0ebdbe3e10c4761d43
村长 <757891685@qq.com>
2023-01-05 20:06:58 +0800

    解决了棋盘渲染和左右移动的问题

---
+++ AuraRevival/AuraRevival/Form3.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace AuraRevival
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Top = new System.Windows.Forms.Panel();
+            this.panel_Left = new System.Windows.Forms.Panel();
+            this.panel_Fill = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_Right = new System.Windows.Forms.Panel();
+            this.panel_Bottom = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel_Fill.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_Top
+            // 
+            this.panel_Top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Top.Location = new System.Drawing.Point(0, 0);
+            this.panel_Top.Name = "panel_Top";
+            this.panel_Top.Size = new System.Drawing.Size(925, 31);
+            this.panel_Top.TabIndex = 0;
+            // 
+            // panel_Left
+            // 
+            this.panel_Left.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Left.Location = new System.Drawing.Point(0, 31);
+            this.panel_Left.Name = "panel_Left";
+            this.panel_Left.Size = new System.Drawing.Size(58, 670);
+            this.panel_Left.TabIndex = 1;
+            // 
+            // panel_Fill
+            // 
+            this.panel_Fill.Controls.Add(this.button8);
+            this.panel_Fill.Controls.Add(this.button7);
+            this.panel_Fill.Controls.Add(this.button6);
+            this.panel_Fill.Controls.Add(this.button5);
+            this.panel_Fill.Controls.Add(this.button1);
+            this.panel_Fill.Controls.Add(this.button4);
+            this.panel_Fill.Controls.Add(this.button3);
+            this.panel_Fill.Controls.Add(this.button2);
+            this.panel_Fill.Controls.Add(this.panel2);
+            this.panel_Fill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Fill.Location = new System.Drawing.Point(58, 31);
+            this.panel_Fill.Name = "panel_Fill";
+            this.panel_Fill.Size = new System.Drawing.Size(792, 670);
+            this.panel_Fill.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(692, 254);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "↓";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(692, 196);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "↑";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(712, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "→";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "←";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(671, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(671, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(47, 37);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(603, 603);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(-950, -950);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2501, 2501);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // panel_Right
+            // 
+            this.panel_Right.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel_Right.Location = new System.Drawing.Point(850, 31);
+            this.panel_Right.Name = "panel_Right";
+            this.panel_Right.Size = new System.Drawing.Size(75, 670);
+            this.panel_Right.TabIndex = 0;
+            // 
+            // panel_Bottom
+            // 
+            this.panel_Bottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_Bottom.Location = new System.Drawing.Point(0, 701);
+            this.panel_Bottom.Name = "panel_Bottom";
+            this.panel_Bottom.Size = new System.Drawing.Size(925, 31);
+            this.panel_Bottom.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(671, 292);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "归零";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 732);
+            this.Controls.Add(this.panel_Fill);
+            this.Controls.Add(this.panel_Right);
+            this.Controls.Add(this.panel_Left);
+            this.Controls.Add(this.panel_Top);
+            this.Controls.Add(this.panel_Bottom);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            this.panel_Fill.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Top;
+        private System.Windows.Forms.Panel panel_Left;
+        private System.Windows.Forms.Panel panel_Fill;
+        private System.Windows.Forms.Panel panel_Right;
+        private System.Windows.Forms.Panel panel_Bottom;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ AuraRevival/AuraRevival/Form3.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuraRevival
+{
+    public partial class Form3 : Form
+    {
+        /// <summary>
+        /// 上次点击的坐标
+        /// </summary>
+        public Coor CoorOld { get; set; } = new Coor(0, 0);
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 页面加载结束
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            //FormRefresh();
+        }
+
+        /// <summary>
+        /// 当鼠标指针位于控件上并按下鼠标键时发生。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int padding = Util.Padding;
+            Coor coor = new Coor(e.X/ padding, e.Y / padding);
+
+            #region 上次点击的位置标绿
+            Graphics g2 = panel1.CreateGraphics();
+            Pen pen2 = new Pen(Color.Green); 
+
+            //废弃上面的方法，直接画矩形
+            g2.DrawRectangle(pen2, CoorOld.Rectangle);
+            g2.Dispose();
+            #endregion
+
+            #region 点击的位置标红
+            Graphics g = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+
+            ////两横
+            //g.DrawLine(pen, coor.Rectangle.Location, new Point(coor.Rectangle.Location.X + 1, coor.Rectangle.Location.Y));
+            //g.DrawLine(pen, new Point(coor.Rectangle.Location.X, coor.Rectangle.Location.Y + 1), new Point(coor.Rectangle.Location.X + 1, coor.Rectangle.Location.Y + 1));
+            ////两竖
+            //g.DrawLine(pen, coor.Rectangle.Location, new Point(coor.Rectangle.Location.X, coor.Rectangle.Location.Y + 1));
+            //g.DrawLine(pen, new Point(coor.Rectangle.Location.X + 1, coor.Rectangle.Location.Y), new Point(coor.Rectangle.Location.X + 1, coor.Rectangle.Location.Y + 1));
+
+            //废弃上面的方法，直接画矩形
+            g.DrawRectangle(pen, coor.Rectangle); 
+            g.Dispose();
+            #endregion
+
+            CoorOld = coor;
+        }
+
+        /// <summary>
+        /// 向左
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int padding = Util.Padding;
+            panel1.Location = new Point(panel1.Location.X + padding, panel1.Location.Y);
+            ButtonRefresh();
+        }
+        /// <summary>
+        /// 向右
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int padding = Util.Padding;
+            panel1.Location = new Point(panel1.Location.X - padding, panel1.Location.Y);
+            ButtonRefresh();
+        }
+        /// <summary>
+        /// 向上
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int padding = Util.Padding;
+            panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + padding);
+            ButtonRefresh();
+        }
+        /// <summary>
+        /// 向下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int padding = Util.Padding;
+            panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - padding);
+            ButtonRefresh();
+        }
+        /// <summary>
+        /// 归零
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            panel1.Location = new Point(0, 0);
+            ButtonRefresh();
+        }
+
+        /// <summary>
+        /// 地图刷新
+        /// </summary>
+        private void FormRefresh()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Green);
+            Pen penRed = new Pen(Color.Red);
+            Coor coor111111 = new Coor(1, 1);
+
+            //框定展示的范围
+            Rectangle rec = new Rectangle(-panel1.Location.X, -panel1.Location.Y, panel2.Width, panel2.Height);
+            int maxHeight = rec.Y + rec.Height;
+            int maxWidth = rec.X + rec.Width;
+
+            //循环绘制多条横线
+            for (int i = rec.Y; i <= maxHeight; i += 25)
+            {
+                g.DrawLine(pen, rec.X, i, maxWidth, i);
+            }
+            //循环绘制多条竖线
+            for (int i = rec.X; i <= maxWidth; i += 25)
+            {
+                g.DrawLine(pen, i, rec.Y, i, maxHeight);
+            }
+
+            //在地图(1,1)的位置放个图片
+            g.DrawImage(Image.FromFile(@".\Image\雪景.png"), coor111111.Rectangle.X, coor111111.Rectangle.Y, coor111111.Rectangle.Width, coor111111.Rectangle.Height);
+
+            //当前选中的地方画矩形
+            g.DrawRectangle(penRed, CoorOld.Rectangle);
+            
+            g.Dispose();
+        }
+
+        /// <summary>
+        /// 刷新按钮
+        /// </summary>
+        private void ButtonRefresh()
+        {
+            int padding = Util.Padding;
+            button1.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+
+            if (panel1.Location.X > (-padding))
+            {
+                button1.Enabled = false;
+            }
+            if (panel1.Location.X < (panel2.Width - panel1.Width - padding))
+            {
+                button5.Enabled = false;
+            }
+            if (panel1.Location.Y > (-padding))
+            {
+                button6.Enabled = false;
+            }
+            if (panel1.Location.Y < (panel2.Height - panel1.Height - padding))
+            {
+                button7.Enabled = false;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            FormRefresh();
+        }
+
+    }
+}
