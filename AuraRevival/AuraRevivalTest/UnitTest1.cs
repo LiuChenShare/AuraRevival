e9bcbe8fdc64a352c0fca3c3bcc701cd56b5ae86
村长 <757891685@qq.com>
2023-01-07 13:56:01 +0800

    增加数秒和相关事件

--- AuraRevival/AuraRevivalTest/UnitTest1.cs
+++ AuraRevival/AuraRevivalTest/UnitTest1.cs
@@ -12,12 +12,18 @@ namespace AuraRevivalTest
         [DataRow(1)]
         public void TestMethod1(int index)
         {
-            Console.WriteLine("Alarm：嘀嘀嘀，水已经 {0} 度了：");
-            System.Diagnostics.Debug.WriteLine("Alarm：嘀嘀嘀，水已经 {0} 度了：");
             AuraRevival.Business.MainGame game = new AuraRevival.Business.MainGame();
+            game.SecondsEvent += ShowMsg;
             game.GameStart();
             Thread.Sleep(200 * 1000);
             Assert.AreEqual(1, index);
+        }
+
+
+        public void ShowMsg(DateTime time)
+        {
+            //Console.WriteLine("ShowMsg：嘀嘀嘀，已经 {0} 了：", time.ToString("yyyy-MM-dd HH:mm:ss"));
+            System.Diagnostics.Debug.WriteLine($"ShowMsg：嘀嘀嘀，已经 {time.ToString("yyyy-MM-dd HH:mm:ss")} 了");
         }
     }
 }
--- AuraRevivalBase/AuraRevival.Business/MainGame.cs
+++ AuraRevivalBase/AuraRevival.Business/MainGame.cs
@@ -1,26 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AuraRevival.Business
 {
     public class MainGame
     {
-        int temperature;                        //水温
-        delegate void BoilHandler(int temp);//声明委托
-        event BoilHandler BoilEvent;//声明事件
+        public delegate void TimeHandler(DateTime time);//声明委托
+        /// <summary>秒事件 </summary>
+        public event TimeHandler SecondsEvent;
+        /// <summary>分事件 </summary>
+        public event TimeHandler MinutesEvent;
+        /// <summary>时事件 </summary>
+        public event TimeHandler HoursEvent;
+        /// <summary>日事件 </summary>
+        public event TimeHandler DaysEvent;
+        /// <summary>月事件 </summary>
+        public event TimeHandler MonthsEvent;
+
+
         //实例化Timer类，设置间隔时间为1秒；
         System.Timers.Timer GameTimer = new System.Timers.Timer(1000);
+        /// <summary>
+        /// 游戏时间
+        /// </summary>
+        DateTime GameDate = new DateTime();
 
         public void GameStart()
         {
-            Alarm alarm = new Alarm();
-
-            BoilEvent += alarm.MakeAlert;            //注册方法
-            //BoilEvent += (new Alarm()).MakeAlert;    //给匿名对象注册方法
-            BoilEvent += Display.ShowMsg;            //注册静态方法
+            //SecondsEvent += ShowMsg;
 
             GameTimer.Elapsed += new System.Timers.ElapsedEventHandler(Execute);//到达时间的时候执行事件；
             GameTimer.AutoReset = true;//设置是执行一次（false）还是一直执行(true)；
@@ -30,48 +36,20 @@ namespace AuraRevival.Business
 
         public void Execute(object source, System.Timers.ElapsedEventArgs e)
         {
-            //GameTimer.Stop(); //先关闭定时器
-            ////MessageBox.Show("OK!");
-            //GameTimer.Start(); //执行完毕后再开启器
-
-            temperature++;
-            //    if (BoilEvent != null)
-            //    { //如果有对象注册
-            //        BoilEvent(temperature);  //调用所有注册对象的方法
-            //    }
-            BoilEvent?.Invoke(temperature);  //调用所有注册对象的方法
-
-            if (temperature > 99)
+            GameDate = GameDate.AddSeconds(1);
+            SecondsEvent?.Invoke(GameDate);
+            if (GameDate.Second == 0)
             {
-                GameTimer.Stop();
+                MinutesEvent?.Invoke(GameDate);
+                if (GameDate.Minute == 0)
+                {
+                    HoursEvent?.Invoke(GameDate);
+                    if (GameDate.Hour == 0)
+                    {
+                        DaysEvent?.Invoke(GameDate);
+                    }
+                }
             }
-        }
-    }
-
-    // 警报器
-    public class Alarm
-    {
-        public void MakeAlert(int param)
-        {
-            if (param > 95)
-            {
-                Console.WriteLine("Alarm：嘀嘀嘀，水已经 {0} 度了：", param);
-                System.Diagnostics.Debug.WriteLine("Alarm：嘀嘀嘀，水已经 {0} 度了：", param);
-            }
-        }
-
-        public void Display(object state)
-        {
-        }
-
-    }
-    // 显示器
-    public class Display
-    {
-        public static void ShowMsg(int param)
-        { //静态方法
-            Console.WriteLine("Display：水快烧开了，当前温度：{0}度。", param);
-            System.Diagnostics.Debug.WriteLine("Display：水快烧开了，当前温度：{0}度。", param);
         }
     }
 
